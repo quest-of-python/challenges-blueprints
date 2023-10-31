029dbda96b9225a896a48de420efeebe43e61439
jszafran <jszafran.pv@gmail.com>
2023-10-31 14:03:56 +0100

    Add blueprint for jjit part one challenge.

---
+++ 0005_justjoinit_data_finding_the_gaps/missing_dates.py
@@ -0,0 +1,11 @@
+import pathlib
+from datetime import date, timedelta
+
+
+def find_missing_dates(
+    input_directory: pathlib.Path,
+    start_date: date,
+    end_date: date,
+) -> str:
+    # TODO: implement your logic here
+    pass
